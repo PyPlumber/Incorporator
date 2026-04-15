4bb93d7afd5517da3111f7b936a80f8045325cc4
Eric Huskey <ericmhuskey@gmail.com>
2026-04-14 21:34:21 -0400

    new structure

---
+++ .venv/src/POKEMON_API.py
@@ -0,0 +1,29 @@
+from src import Incorporator
+
+PokemonBase = Incorporator.Incorporator.incSubCls('PokemonBase', 'id', 'name',
+    'https://pokeapi.co/api/v2/',
+                                                  codeAdds= {},
+                                                  exclAdds=[],
+                                                  convAdds={
+        'url': lambda x: PokemonBase.getCodeFromUrl(x)
+    },
+                                                  nameAdds={})
+
+Language = PokemonBase.incSubCls('Language','url', 'name',
+    PokemonBase.endpointAPI+'language/',
+    codeAdds= {},
+    exclAdds=[],
+    convAdds={
+    },
+    nameAdds={})
+
+PokeSpecies = PokemonBase.incSubCls('PokeSpecies','url', 'name',
+    PokemonBase.endpointAPI+'pokemon-species/',
+    codeAdds= {},
+    exclAdds=[],
+    convAdds={
+    },
+    nameAdds={})
+
+
+## :"https://pokeapi.co/api/v2/ability/?limit=20&offset=20"
