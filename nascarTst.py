22773e62ef05e75098cb459103136382ba54e7e8
Eric Huskey <ericmhuskey@gmail.com>
2026-04-14 20:58:37 -0400

    new structure

--- nascarTst.py
+++
@@ -1,9 +0,0 @@
-import NASCAR_API
-
-trackList  = TrackBase.refreshDataREST(TrackBase.endpointAPI, rPath='items', nextUrlPath=['next'])
-nascrRaces = NascarRaceBase.refreshDataREST(NascarRaceBase.endpointAPI, rPath='series_1', nextUrlPath=['next'])
-buschRaces = BuschRaceBase.refreshDataREST(BuschRaceBase.endpointAPI, rPath='series_2', nextUrlPath=['next'])
-truckRaces = TruckRaceBase.refreshDataREST(TruckRaceBase.endpointAPI, rPath='series_3', nextUrlPath=['next'])
-
-
-print(len(nascrRaces))
